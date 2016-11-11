cce410df65ade3a4b2e4f88de3dfbf41994ab67d
nmbrone <sergey.snozyk@gmail.com>
2016-11-12 00:54:38 +0200

    done validation workflow for formfield

--- src/js/classes/form-field.js
+++ src/js/classes/form-field.js
@@ -1,131 +1,186 @@
-const EMAIL_RE = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+// as example
+// const EMAIL_RE = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
 
-const RULES = {
-  'email': (s) => EMAIL_RE.test(s),
-  'min-length': (s, len) => s.length >= len,
-  'max-length': (s, len) => s.length <= len,
-  'required': (s) => !!s.length,
-  'default': () => true
-};
 
-const ERROR_MESSAGES = {
-  'email': () => 'Email is not valid',
-  'min-length': (n) => `Value must contain at least ${n} characters`,
-  'max-length': (n) => `Value can\'t contain more than ${n} characters`,
-  'required': () => 'Field is required',
-  'default': () => 'Unknown error'
-};
+const FormField = (function() {
 
-class FormField {
-  constructor(element, props = {}) {
-    this.element   = isDomElement(element) ? element : qs(element);
-    this.props     = Object.assign({}, FormField.defaults, props);
-    this._isValid  = null;
-    this._hasError = null;
+  const EMAIL_RE = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
-    if (this.element.matches(this.props.control)) {
-      this.control = this.element;
-    } else {
-      this.control = qs(this.props.control, this.element);
+  const NUMBER_RE = /^[\-\+]?(\d+|\d+\.?\d+)$/;
+
+  const RULE_PARAM_RE = /\[.*\]$/; // match 'ruleName[param1, param2]'
+
+  const parseRule = (str) => {
+    const match = str.match(RULE_PARAM_RE);
+    const name = str.replace(RULE_PARAM_RE, '');
+    const params = match ? match[0].slice(1, -1).split(',').filter(p => !!p) : [];
+    return { name, params };
+  };
+
+  window.parse = parseRule;
+
+  const messages = {
+    email: 'Email is not valid',
+    minLength: 'Value must contain at least {n} characters',
+    maxLength: 'Value can\'t contain more than {n} characters',
+    required: 'Field is required',
+  };
+
+  const validator = {
+    email: (s) => EMAIL_RE.test(s) || messages.email,
+    minLength: (s, len = 6) => s.length >= len || messages.minLength.replace('{n}', len),
+    maxLength: (s, len = 100) => s.length <= len || messages.maxLength.replace('{n}', len),
+    required: (s) => !!s.length || messages.required,
+  };
+
+  class FormField {
+    constructor(element, props = {}) {
+      this.element   = isDomElement(element) ? element : qs(element);
+      this.props     = Object.assign({}, FormField.defaults, props);
+      this._isValid  = null;
+      this._hasError = null;
+
+      if (this.element.matches(this.props.control)) {
+        this.control = this.element;
+      } else {
+        this.control = qs(this.props.control, this.element);
+      }
+
+      if (this.control !== this.element) {
+        this._setupErrorElement();
+      }
+
+      this._setupValidator();
+      this.validate = this.validate.bind(this);
+      this._bindEvents();
+
+      console.log(this);
     }
 
-    if (this.control !== this.element) {
-      this._setupErrorElement();
+    validate() {
+      const result = this._validator(this.control.value);
+      this._resetState();
+
+      if (result === true) {
+        this._setValidState();
+      } else {
+        this._setErrorState(result);
+      }
     }
 
-    this._setupValidator();
-    this.validate = this.validate.bind(this);
-    this._bindEvents();
-
-    console.log(this);
-  }
-
-  validate() {
-    const isValid = this._validator(this.control.value);
-    this._resetState();
-
-    if (isValid) {
-      this._setValidState();
-    } else {
-      this._setErrorState();
+    _setErrorState(message) {
+      this.errorMessages = [].concat(message);
+      this.element.classList.add(this.props.errorClass);
+      this.element.appendChild(this.errorElement);
+      if (this.errorElement) this.errorElement.innerHTML = this.errorMessages.join('<br>');
+      this._hasError = true;
     }
 
-    return isValid;
-  }
-
-  _setErrorState() {
-    this.element.classList.add(this.props.errorClass);
-    this.element.appendChild(this.errorElement);
-    this._hasError = true;
-  }
-
-  _setValidState() {
-    this.element.classList.add(this.props.validClass);
-    this._isValid = true;
-  }
-
-  _resetState() {
-    if (this._isValid === null && this._hasError === null) return;
-
-    const { errorClass, validClass } = this.props;
-    this.element.classList.remove(errorClass);
-    this.element.classList.remove(validClass);
-
-    // unmount error element if mounted
-    if (this.errorElement && this.errorElement.parentNode) {
-      this.element.removeChild(this.errorElement);
+    _setValidState() {
+      this.element.classList.add(this.props.validClass);
+      this._isValid = true;
     }
 
-    this._isValid = null;
-    this._hasError = null;
-  }
+    _resetState() {
+      if (this._isValid === null && this._hasError === null) return;
 
-  _setupErrorElement() {
-    if (this.errorElement) return;
-    this.errorElement = document.createElement('span');
-    this.errorElement.className = this.props.errorElementClass;
-  }
+      const { errorClass, validClass } = this.props;
+      this.element.classList.remove(errorClass);
+      this.element.classList.remove(validClass);
 
-  _bindEvents() {
-    const { resetOnFocus, validateOnInput, validateOnBlur } = this.props;
+      // unmount error element if mounted
+      if (this.errorElement && this.errorElement.parentNode) {
+        this.errorElement.innerHTML = '';
+        this.element.removeChild(this.errorElement);
+      }
 
-    if (resetOnFocus) {
-      this.control.addEventListener('focus', () => this._resetState());
+      this.errorMessages = [];
+
+      this._isValid = null;
+      this._hasError = null;
     }
 
-    if (validateOnInput) {
-      this.control.addEventListener('input', this.validate);
+    _setupErrorElement() {
+      if (this.errorElement) return;
+      this.errorElement = document.createElement('span');
+      this.errorElement.className = this.props.errorElementClass;
     }
 
-    if (validateOnBlur) {
-      this.control.addEventListener('blur', this.validate);
+    _bindEvents() {
+      const { resetOnFocus, validateOnInput, validateOnBlur } = this.props;
+
+      if (resetOnFocus) {
+        this.control.addEventListener('focus', () => this._resetState());
+      }
+
+      if (validateOnInput) {
+        this.control.addEventListener('input', this.validate);
+      }
+
+      if (validateOnBlur) {
+        this.control.addEventListener('blur', this.validate);
+      }
     }
+
+    _setupValidator() {
+      const { validate, customValidator } = this.props;
+      const type = typeof validate;
+      let rules  = [];
+      let fns    = [];
+
+      if (typeof customValidator === 'function') {
+        fns.push(customValidator);
+      }
+
+      if (type === 'string' || Array.isArray(validate)) {
+        rules = rules.concat(validate);
+      }
+
+      rules.forEach(rule => {
+        const { name, params } = parseRule(rule);
+        const fn = validator[name];
+        if (fn) {
+          fns.push(fn.bind(undefined, ...params));
+          // fns.push(() => fn.apply(undefined, params));
+        }
+      });
+
+      // if "customValidator" is not a function or "validate" has unknown value
+      if (!fns.length) {
+        throw new Error(
+          'You must provide "validate" or "customValidator" option for FormField class.'
+        );
+      }
+
+      this._rules = fns;
+
+      this._validator = (value) => {
+        const results = this._rules.map(fn => fn(value));
+        const valid = results.every(res => res === true);
+        return valid || results.filter(res => typeof res !== 'boolean');
+      };
+    }
+
   }
 
-  _setupValidator() {
-    if (typeof this.props.customValidator === 'function') {
-      return this._validator = this.props.customValidator.bind(this);
-    }
+  FormField.defaults = {
+    errorClass: 'has-error',
+    validClass: 'has-success',
+    errorElementClass: 'text-danger',
+    resetOnFocus: true,
+    validateOnInput: false,
+    validateOnBlur: true,
+    control: 'input',
+    customValidator: null,
+    validate: 'required'
+  };
 
-    const rules = [].concat(this.props.validate);
-    const validators = rules.map(rule => RULES[rule] || RULES['default']);
-    this._validator = (s) => validators.map(fn => fn(s)).every(res => res === true);
-  }
+  return FormField;
 
-}
-
-FormField.defaults = {
-  errorClass: 'has-error',
-  validClass: 'has-success',
-  errorElementClass: 'text-error',
-  resetOnFocus: true,
-  validateOnInput: false,
-  validateOnBlur: true,
-  control: 'input',
-  customValidator: null,
-  validate: 'required'
-};
+} ());
 
 setTimeout(() => {
-  new FormField('#public-info .form-group');
+  new FormField('#public-info .form-group', {
+    validate: ['required', 'email']
+  });
 }, 1000);
