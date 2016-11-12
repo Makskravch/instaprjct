2a926a7c501fb7cb75ba65aa91215db89f97f506
nmbrone <sergey.snozyk@gmail.com>
2016-11-12 23:50:36 +0200

    matches polifyll from MDN

--- src/js/lib/matches-polyfill.js
+++ src/js/lib/matches-polyfill.js
@@ -1,14 +1,9 @@
-if (!Element.prototype.matches) {
-  Element.prototype.matches =
-    Element.prototype.matchesSelector ||
-    Element.prototype.mozMatchesSelector ||
-    Element.prototype.msMatchesSelector ||
-    Element.prototype.oMatchesSelector ||
-    Element.prototype.webkitMatchesSelector ||
-    function(s) {
-      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-        i = matches.length;
-      while (--i >= 0 && matches.item(i) !== this) {}
-      return i > -1;
-    };
-}
+(function(e) {
+  e.matches || (e.matches = e.matchesSelector || function(selector) {
+    var matches = document.querySelectorAll(selector), th = this;
+
+    return Array.prototype.some.call(matches, function(e) {
+      return e === th;
+    });
+  });
+}) (Element.prototype);
