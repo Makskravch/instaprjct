75b9b38a16fcf52003ddff0e47fd92ae024af321
nmbrone <sergey.snozyk@gmail.com>
2016-11-13 17:55:27 +0200

    test VForm

--- src/js/routes/profile-edit.js
+++ src/js/routes/profile-edit.js
@@ -1,6 +1,34 @@
 function profileEdit(ctx, next) {
   render('profile-edit');
 
+  // init picture uploader
   new ProfilePictureUploader('#profile-picture');
-  // new UserProfile('#edit-profile');
+
+  const publicInfoForm     = document.forms['public-info'];
+  const NAME_VALIDATOR_RE  = /^\w+\s*\w*$/;
+  const PHONE_VALIDATOR_RE = /^\d{10,12}$/;
+
+  new VForm(publicInfoForm, {
+    onValid(f) {
+      console.log(f.serialize());
+    },
+    fields: {
+      'public_email': {
+        validate: 'email'
+      },
+      'display_name': {
+        validate: 'required',
+        customValidator(value) {
+          return NAME_VALIDATOR_RE.test(value)
+            || 'Field can contain only letters, numbers, or underscore sign';
+        }
+      },
+      'phone_number': {
+        customValidator(value) {
+          return PHONE_VALIDATOR_RE.test(value)
+            || 'Phone must contain from 10 to 12 digits';
+        }
+      }
+    }
+  });
 }
