1e621cfcb004384ea1f186f76785fffbbcc0fbe0
nmbrone <sergey.snozyk@gmail.com>
2017-01-08 00:56:43 +0200

    upd profile edit screen

--- src/js/routes/profile-edit.js
+++ src/js/routes/profile-edit.js
@@ -7,27 +7,27 @@ function profileEdit(ctx, next) {
   new ProfilePictureUploader('#profile-picture');
 
   const publicInfoForm     = document.forms['public-info'];
-  const NAME_VALIDATOR_RE  = /^\w+\s*\w*$/;
   const PHONE_VALIDATOR_RE = /^\+?\d{10,12}$/;
 
   function updateUserInfo(newData) {
     const user  = firebase.auth().currentUser;
     const dbRef = firebase.database().ref(`users/${user.uid}`);
-    const info  = pick(newData, ['displayName', 'publicEmail', 'phoneNumber', 'social', 'about']);
+    const info  = pick(newData, ['displayName', 'phoneNumber', 'social', 'about']);
     const { displayName } = info;
+
     if (isEmptyObject(info)) {
       return Promise.resolve();
     }
+
     if (displayName !== undefined) {
       user.updateProfile({ displayName }).catch(err => console.log(err));
     }
-    console.log(info);
+
     return dbRef.transaction((data) => Object.assign({}, data, info));
   }
 
   function submitForm(f) {
     const data = f.serialize();
-    console.log(data);
 
     if (isEmptyObject(data)) {
       return f.setInvalidState();
@@ -36,8 +36,7 @@ function profileEdit(ctx, next) {
     f.setLoadingState();
 
     updateUserInfo(data)
-      .then((...args) => {
-        console.log(args);
+      .then(() => {
         f.resetState().setSuccessState();
       })
       .catch(err => {
@@ -49,16 +48,6 @@ function profileEdit(ctx, next) {
   new VForm(publicInfoForm, {
     onValid: submitForm,
     fields: {
-      'publicEmail': {
-        validate: 'email'
-      },
-      'displayName': {
-        // validate: 'required',
-        customValidator(value) {
-          return NAME_VALIDATOR_RE.test(value)
-            || 'Field can contain only letters, numbers, or underscore sign';
-        }
-      },
       'phoneNumber': {
         customValidator(value) {
           return PHONE_VALIDATOR_RE.test(value)
